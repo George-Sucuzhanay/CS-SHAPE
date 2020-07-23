e768c0102b18ee44ef85be43ee6cbe74ea54207e
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-07-22 20:14:50 -0400

    Create CS-GroupA.py

---
+++ CS-GroupA.py
@@ -0,0 +1,53 @@
+# seven equals 111, nine equals 1001,  100100
+# 1011 in decimal is 11, 11100 is 28
+# 4 base 2 is 100, 5 base 2 is 101, 6 is 110, 7 is 111, 8 is 1000, 9 is 1001, 10 is 1010
+
+#Question 5
+print ("Hello SHAPE!")
+5+2
+5%2
+5//2
+5/2
+2*5
+2**5
+
+#Question 6
+print("Hello, my name is Ludmilla. I'm 15 years old")
+print('Hello, my name is Zack. I am 15 years old.')
+print ("Hello, my name is Sarah. I am 15 years old.")
+print("Hello, my nsmr id George. I am 16 years old.")
+
+#nursery rhyme
+print( 7**4 +2 )
+
+#rectangle
+print('**************************')
+print('*                        *')
+print('*                        *')
+print('**************************')
+      
+#Question 9
+name = input("Please enter your name:")
+age = input("Please enter your age:")
+print('Your name is:')
+print(name)
+print('Your age is:')
+print(age)
+
+#Question 10
+duration = int(input('Enter a duration in seconds'))
+
+
+
+seconds = int(duration%60)
+total_minutes = int(duration//60)
+hours = int(total_minutes//60)
+minutes = int(total_minutes%60)
+
+print(hours)
+print('hour(s)')
+print(minutes)
+print('minute(s)')
+print(seconds)
+print('second(s)')
+
